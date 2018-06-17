6c23c3b15bb2cb4a26e1e923d29cfcfd0e79c287
mathieudutour <mathieu@dutour.me>
2018-06-17 15:02:17 -0700

    version can only have 2 digits

--- scripts/generate-version.js
+++ scripts/generate-version.js
@@ -4,9 +4,4 @@ var y = date.getUTCFullYear() - 2000
 var m = date.getUTCMonth() + 1
 var d = date.getUTCDate()
 
-var divider = (86400 / 255)
-var seconds = (date.getUTCHours() * 3600) + (date.getUTCMinutes() * 60) + date.getUTCSeconds()
-
-var i = 1 + Math.floor(seconds / divider)
-
-console.log(y + '.' + m + '.' + d + '000' + i)
+console.log(y + '.' + m + '.' + d)
